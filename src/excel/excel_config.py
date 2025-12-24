b8664406171fe29a82f0b6946e9a57da014cea3f
David Liu <31893484+davidliudev@users.noreply.github.com>
2025-12-24 09:02:35 +0800

    Remove unrelated function except for read_xlsx_metadata

--- src/excel/excel_config.py
+++ src/excel/excel_config.py
@@ -2,19 +2,14 @@ import os
 
 # list all include directories
 include_directories = [
-    os.path.sep.join(x.split('/')) for x in ['src/excel/include', 'src/excel/numformat/include']
+    os.path.sep.join(x.split('/')) for x in ['src/excel/include']
 ]
 # source files
 source_files = [os.path.sep.join(x.split('/')) for x in ['src/excel/excel_extension.cpp']]
 source_files += [
     os.path.sep.join(x.split('/'))
     for x in [
-        'src/excel/numformat/nf_calendar.cpp',
-        'src/excel/numformat/nf_localedata.cpp',
-        'src/excel/numformat/nf_zformat.cpp',
-        'src/excel/xlsx/read_xlsx.cpp',
         'src/excel/xlsx/read_xlsx_metadata.cpp',
-        'src/excel/xlsx/copy_xlsx.cpp',
         'src/excel/xlsx/zip_file.cpp',
     ]
 ]
--- src/excel/excel_extension.cpp
+++ src/excel/excel_extension.cpp
@@ -1,89 +1,17 @@
 #include "excel_metadata_extension.hpp"
 
 #include "duckdb.hpp"
-#include "duckdb/common/exception.hpp"
-#include "duckdb/common/string_util.hpp"
-#include "duckdb/function/scalar_function.hpp"
 #include "duckdb/main/extension/extension_loader.hpp"
-#include "nf_calendar.h"
-#include "nf_localedata.h"
-#include "nf_zformat.h"
-#include "xlsx/read_xlsx.hpp"
 #include "xlsx/read_xlsx_metadata.hpp"
 
-#include <duckdb/common/types/time.hpp>
-
 namespace duckdb {
-
-static std::string GetNumberFormatString(std::string &format, double num_value) {
-	duckdb_excel::LocaleData locale_data;
-	duckdb_excel::ImpSvNumberInputScan input_scan(&locale_data);
-	uint16_t nCheckPos;
-	std::string out_str;
-
-	duckdb_excel::SvNumberformat num_format(format, &locale_data, &input_scan, nCheckPos);
-
-	if (!num_format.GetOutputString(num_value, out_str)) {
-		return out_str;
-	}
-
-	return "";
-}
-
-static string_t NumberFormatScalarFunction(Vector &result, double num_value, string_t format) {
-	try {
-		string in_str = format.GetString();
-		string out_str = GetNumberFormatString(in_str, num_value);
-
-		if (out_str.length() > 0) {
-			auto result_string = StringVector::EmptyString(result, out_str.size());
-			auto result_data = result_string.GetDataWriteable();
-			memcpy(result_data, out_str.c_str(), out_str.size());
-			result_string.Finalize();
-			return result_string;
-		} else {
-			auto result_string = StringVector::EmptyString(result, 0);
-			result_string.Finalize();
-			return result_string;
-		}
-	} catch (...) {
-		throw InternalException("Unexpected result for number format");
-	}
-
-	return string_t();
-}
-
-static void NumberFormatFunction(DataChunk &args, ExpressionState &state, Vector &result) {
-	auto &number_vector = args.data[0];
-	auto &format_vector = args.data[1];
-	BinaryExecutor::Execute<double, string_t, string_t>(
-	    number_vector, format_vector, result, args.size(),
-	    [&](double value, string_t format) { return NumberFormatScalarFunction(result, value, format); });
-}
-
-//--------------------------------------------------------------------------------------------------
-// Time conversion functions
-//--------------------------------------------------------------------------------------------------
 
 //--------------------------------------------------------------------------------------------------
 // Load
 //--------------------------------------------------------------------------------------------------
 
 static void LoadInternal(ExtensionLoader &loader) {
-
-	ScalarFunction text_func("text", {LogicalType::DOUBLE, LogicalType::VARCHAR}, LogicalType::VARCHAR,
-							 NumberFormatFunction);
-	loader.RegisterFunction(text_func);
-
-	ScalarFunction excel_text_func("excel_text", {LogicalType::DOUBLE, LogicalType::VARCHAR}, LogicalType::VARCHAR,
-								   NumberFormatFunction);
-
-	loader.RegisterFunction(excel_text_func);
-
-	// Register the XLSX functions
-	ReadXLSX::Register(loader);
 	ReadXLSXMetadata::Register(loader);
-	WriteXLSX::Register(loader);
 }
 
 void ExcelMetadataExtension::Load(ExtensionLoader &loader) {
